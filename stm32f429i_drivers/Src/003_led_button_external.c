9969f6abb4d212c3a19f54e471c6c0f41172fa1c
qwe111845 <qwe111845@gmail.com>
2022-05-25 16:58:15 +0800

    feat: LEG 閃爍的程式
    新增原因:
    1 測試GPIO的輸出輸入，使用外部按紐接收input取輸出外部LED燈泡亮不亮。
    2 查 board user manual 使用GPIOB pin 12 接收，及LED 的GPIOA pin 8 輸出。

---
+++ stm32f429i_drivers/Src/003_led_button_external.c
@@ -0,0 +1,52 @@
+#include <stdint.h>
+#include "stm32f429i.h"
+
+#define HIGH        ENABLE
+#define LOW         DISABLE
+
+#define BTN_PRESSED LOW
+
+void delay(void) {
+	for (uint32_t i = 0; i < 300000; i++);
+}
+
+int main(void)
+{
+
+
+	GPIO_Handle_t GPIOLed;
+
+	GPIOLed.pGPIO = GPIOA;
+	GPIOLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_8;
+	GPIOLed.GPIO_PinConfig.GPIO_PinMode   = GPIO_MODE_OUTPUT;
+	GPIOLed.GPIO_PinConfig.GPIO_PinSpeed  = GPIO_SPEED_FAST;
+	GPIOLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIOLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClocckControl(GPIOA, ENABLE);
+
+	GPIO_Init(&GPIOLed);
+
+	GPIO_Handle_t GPIOButton;
+
+	GPIOButton.pGPIO = GPIOB;
+	GPIOButton.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GPIOButton.GPIO_PinConfig.GPIO_PinMode   = GPIO_MODE_INPUT;
+	GPIOButton.GPIO_PinConfig.GPIO_PinSpeed  = GPIO_SPEED_FAST;
+	GPIOButton.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PU;
+
+	GPIO_PeriClocckControl(GPIOB, ENABLE);
+
+	GPIO_Init(&GPIOButton);
+
+
+	while (1) {
+		if (GPIO_ReadFromInputPin(GPIOB, GPIO_PIN_NO_12) == BTN_PRESSED) {
+			delay();
+			GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_8);
+		}
+	}
+
+
+	return 0;
+}
