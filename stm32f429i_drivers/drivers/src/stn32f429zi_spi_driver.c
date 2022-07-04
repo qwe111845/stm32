557d714be36e9b776c610e2fe1f223b36f06d175
qwe111845 <qwe111845@gmail.com>
2022-07-04 11:33:35 +0800

    feat: Serial Peripheral API-SPI_ReceiveData實作
    修改原因:
    1. 實作 SPI_ReceiveData 測試接收 Arduino 回傳的資料。
    
    修改項目:
    1. SPI_ReceiveData() 與 SPI_SendData() 幾乎一樣，只是改成Rxbuffer，讀取DR中的資料。

--- stm32f429i_drivers/drivers/src/stn32f429zi_spi_driver.c
+++ stm32f429i_drivers/drivers/src/stn32f429zi_spi_driver.c
@@ -247,7 +247,36 @@ void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t length) {
  *
  * @Note              - none
  */
-void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t * length);
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t length) {
+
+	// Check data length is greater than zero
+		while (length > 0) {
+
+			// 1. Wait until RXNE is set
+			while (SPI_GetFlagStatus(pSPIx, SPI_RXNE_FLAG) == FLAG_RESET);
+
+			// 2. Check the DFF bit in CR1
+			if ((pSPIx->CR1 & (1 << SPI_CR1_DFF))) {
+
+				// 16 bit DFF
+				// 1. load the data from DR to Rxbuffer address
+				*((uint16_t *)pRxBuffer) = pSPIx->DR;
+				length--;
+				length--;
+				(uint16_t *)pRxBuffer++;
+
+			} else {
+
+				// 8 bit DFF
+				*(pRxBuffer) = pSPIx->DR;
+				length--;
+				pRxBuffer++;
+
+			}
+
+		}
+
+}
 
 
 /*
