d7cbc3f6062242790827e5282689f5885d25c830
qwe111845 <qwe111845@gmail.com>
2022-07-18 17:10:51 +0800

    feat: Serial Peripheral API-SPI 各 API 實作
    新增原因:
    1. 實作 SPI interrupt 需要的 function 及增加 SPI_Handle_t 的變數方便中斷時處理。
    
    新增項目:
    1. 新增並實作 spi_txe_interrupt_handle()、spi_rxne_interrupt_handle()、spi_ovr_err_interrupt_handle() 做傳送、接收資料及 OVR err 中斷後的處理，直到處理完 call SPI_ApplicationEventCallback() 做該 Event 相關的動作。
    2. 實作 SPI_SendDataIT() 當中斷時儲存要傳送的資料及長度至SPI_Handle_t 的 global 變數，標記 TxState 為 BUSY，並將 TXNEIE enable，則會觸發 Interrupt，資料傳輸交由對應的 ISR code 處理 。
    3. 實作 SPI_ReceiveDataIT()，當中斷時儲存要接收的資料及長度至SPI_Handle_t 的 global 變數，標記 RxState 為 BUSY，並將 RXNEIE enable，則會觸發 Interrupt，資料傳輸交由對應的 ISR code 處理 。
    4. 實作 SPI_IRQHandling() ，檢查是哪個 error 及是否需要處理 Interrupt，丟到對應的 interrupt_handle() 處理。
    5. 實作 SPI_ClearOVRFlag()、CloseTransmission()、CloseReception()，處理完收送資料後解除Tx, Rx, OVR 的Busy。
    6. SPI_ApplicationEventCallback() 交給 program 實作，就看是接到甚麼 Events 就做甚麼動作。

--- stm32f429i_drivers/drivers/src/stn32f429zi_spi_driver.c
+++ stm32f429i_drivers/drivers/src/stn32f429zi_spi_driver.c
@@ -7,6 +7,9 @@
 
 #include "stm32f429zi_spi_driver.h"
 
+static void spi_txe_interrupt_handle(SPI_Handle_t *pSPIHandle);
+static void spi_rxne_interrupt_handle(SPI_Handle_t *pSPIHandle);
+static void spi_ovr_err_interrupt_handle(SPI_Handle_t *pSPIHandle);
 
 /*
  * Peripheral Clock setup
@@ -163,17 +166,16 @@ void SPI_DeInit(SPI_RegDef_t *pSPIx) {
 }
 
 
-
 /*********************************************************************
- * @fn      		  - SPI_SendData
+ * @fn      		  - SPI_GetFlagStatus
  *
- * @brief             - This function gets status from status register by given FlagName
+ * @brief             - This function return status from status register by given FlagName
  *
  * @param[in]         - base address of the SPI peripheral
  * @param[in]         - position of SR flag
  * @param[in]         -
  *
- * @return            - none
+ * @return            - Flag status
  *
  * @Note              - none
  */
@@ -193,10 +195,10 @@ uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t FlagName) {
 /*********************************************************************
  * @fn      		  - SPI_SendData
  *
- * @brief             - This function sends data from given SPI
+ * @brief             - This function sends data to given SPI MOSI pin
  *
  * @param[in]         - base address of the SPI peripheral
- * @param[in]         - data to send
+ * @param[in]         - data to be transmitted
  * @param[in]         - length of data
  *
  * @return            - none
@@ -237,11 +239,11 @@ void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t length) {
 /*********************************************************************
  * @fn      		  - SPI_ReceiveData
  *
- * @brief             - This function enables or disables peripheral clock for the given SPI port
+ * @brief             - This function reads data from given SPI MISO pin
  *
  * @param[in]         - base address of the SPI peripheral
- * @param[in]         - ENABLE or DISABLE macros
- * @param[in]         -
+ * @param[in]         - received data
+ * @param[in]         - length of data
  *
  * @return            - none
  *
@@ -286,9 +288,9 @@ void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t length) {
 /*********************************************************************
  * @fn      		  - SPI_IRQInterruptConfig
  *
- * @brief             - This function enables or disables peripheral clock for the given SPI port
+ * @brief             - This function changes configuration of IRQ ISER
  *
- * @param[in]         - base address of the SPI peripheral
+ * @param[in]         - IRQNumber
  * @param[in]         - ENABLE or DISABLE macros
  * @param[in]         -
  *
@@ -296,12 +298,155 @@ void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t length) {
  *
  * @Note              - none
  */
-void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t enOrDi);
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t enOrDi)
+{
+
+    if(enOrDi == ENABLE) {
+
+        if(IRQNumber <= 31) {
+
+            //program ISER0 register
+            *NVIC_ISER0 |= ( 1 << IRQNumber );
+
+        } else if(IRQNumber > 31 && IRQNumber < 64 )  { //32 to 63
+
+            //program ISER1 register
+            *NVIC_ISER1 |= ( 1 << (IRQNumber % 32) );
+
+        } else if(IRQNumber >= 64 && IRQNumber < 96 ) {
+
+            //program ISER2 register //64 to 95
+            *NVIC_ISER3 |= ( 1 << (IRQNumber % 64) );
+
+        }
+
+    } else {
+
+        if(IRQNumber <= 31) {
+
+            //program ICER0 register
+            *NVIC_ICER0 |= ( 1 << IRQNumber );
+
+        } else if(IRQNumber > 31 && IRQNumber < 64 ) {
+
+            //program ICER1 register
+            *NVIC_ICER1 |= (1 << (IRQNumber % 32));
+
+        } else if(IRQNumber >= 6 && IRQNumber < 96 ) {
+
+            //program ICER2 register
+            *NVIC_ICER3 |= ( 1 << (IRQNumber % 64) );
+
+        }
+
+    }
+
+}
 
 
 /*********************************************************************
  * @fn      		  - SPI_IRQPriorityConfig
  *
+ * @brief             - This function changes priority of IRQ
+ *
+ * @param[in]         - IRQ number
+ * @param[in]         - IRQ priority
+ * @param[in]         -
+ *
+ * @return            - none
+ *
+ * @Note              - none
+ */
+void SPI_IRQPriorityConfig(uint8_t IRQNumber,uint32_t IRQPriority)
+{
+    //1. first lets find out the ipr register
+    uint8_t iprx = IRQNumber / 4;
+    uint8_t iprx_section  = IRQNumber % 4 ;
+
+    uint8_t shift_amount = ( 8 * iprx_section) + ( 8 - NO_PR_BITS_IMPLEMENTED) ;
+
+    *(  NVIC_PR_BASE_ADDR + iprx ) |=  ( IRQPriority << shift_amount );
+
+}
+
+
+/*********************************************************************
+ * @fn      		  - SPI_SendDataIT
+ *
+ * @brief             - API to send data with interrupt mode
+ *
+ * @param[in]         - base address of the SPI peripheral and SPI configuration
+ * @param[in]         - data to be transmitted
+ * @param[in]         - length of data
+ *
+ * @return            - Tx State
+ *
+ * @Note              - none
+ */
+uint8_t SPI_SendDataIT(SPI_Handle_t *pSPIHandle, uint8_t *pTxBuffer, uint32_t length) {
+
+    uint8_t state = pSPIHandle->TxState;
+
+    if (state != SPI_BUSY_IN_TX) {
+
+        // 1. Save the Tx buffer address and length information in some global variables
+        pSPIHandle->pTxBuffer = pTxBuffer;
+        pSPIHandle->TxLength = length;
+
+        // 2. Mark the SPI state as busy in transmission so that
+        //    no other code can take over same SPI peripheral until transmission is over
+        pSPIHandle->TxState = SPI_BUSY_IN_TX;
+
+        // 3. Enable the TXEIE control bit to get interrupt whenever TXE flag is set in SR
+        pSPIHandle->pSPI->CR2 |= (1 << SPI_CR2_TXNEIE);
+
+
+    }
+
+    // 4. Data Transmission will be handled by the ISR code ( will implement later)
+    return state;
+}
+
+
+/*********************************************************************
+ * @fn      		  - SPI_ReceiveDataIT
+ *
+ * @brief             - API to receive data with interrupt mode
+ *
+ * @param[in]         - base address of the SPI peripheral and SPI configuration
+ * @param[in]         - data to be received
+ * @param[in]         - length of data
+ *
+ * @return            - Rx State
+ *
+ * @Note              - none
+ */
+uint8_t SPI_ReceiveDataIT(SPI_Handle_t *pSPIHandle, uint8_t *pRxBuffer, uint32_t length) {
+
+    uint8_t state = pSPIHandle->RxState;
+
+    if (state != SPI_BUSY_IN_RX) {
+
+        // 1. Save the Tx buffer address and length information in some global variables
+        pSPIHandle->pRxBuffer = pRxBuffer;
+        pSPIHandle->RxLength = length;
+
+        // 2. Mark the SPI state as busy in transmission so that
+        //    no other code can take over same SPI peripheral until transmission is over
+        pSPIHandle->RxState = SPI_BUSY_IN_RX;
+
+        // 3. Enable the RXNEIE control bit to get interrupt whenever TXE flag is set in SR
+        pSPIHandle->pSPI->CR2 |= (1 << SPI_CR2_RXNEIE);
+
+    }
+
+    return state;
+
+}
+
+/*********************************************************************
+ * @fn      		  - SPI_IRQHandling
+ *
  * @brief             - This function enables or disables peripheral clock for the given SPI port
  *
  * @param[in]         - base address of the SPI peripheral
@@ -312,7 +457,201 @@ void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t enOrDi);
  *
  * @Note              - none
  */
-void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+void SPI_IRQHandling(SPI_Handle_t *pSPIHandle) {
+
+    uint8_t error, needToHandle;
+
+    // check for TXE
+    error = pSPIHandle->pSPI->SR & (1 << SPI_SR_TXE);
+    needToHandle = pSPIHandle->pSPI->CR2 & (1 << SPI_CR2_TXNEIE);
+
+    if (error & needToHandle) {
+
+        // handle TXE
+        spi_txe_interrupt_handle(pSPIHandle);
+
+    }
+
+    // check for RXE
+    error = pSPIHandle->pSPI->SR & (1 << SPI_SR_RXNE);
+    needToHandle = pSPIHandle->pSPI->CR2 & (1 << SPI_CR2_RXNEIE);
+
+    if (error & needToHandle) {
+
+        // handle RXE
+        spi_rxne_interrupt_handle(pSPIHandle);
+
+    }
+
+    // check for OVR flag
+    error = pSPIHandle->pSPI->SR & (1 << SPI_SR_RXNE);
+    needToHandle = pSPIHandle->pSPI->CR2 & (1 << SPI_CR2_RXNEIE);
+
+    if (error & needToHandle) {
+
+        // handle OVR
+        spi_ovr_err_interrupt_handle(pSPIHandle);
+
+    }
+
+}
+
+static void spi_txe_interrupt_handle(SPI_Handle_t *pSPIHandle) {
+
+    // 2. Check the DFF bit in CR1
+    if ((pSPIHandle->pSPI->CR1 & (1 << SPI_CR1_DFF))) {
+
+        // 16 bit DFF
+        // 1. load the data in to the DR
+        pSPIHandle->pSPI->DR = *((uint16_t *)pSPIHandle->pTxBuffer);
+        pSPIHandle->TxLength--;
+        pSPIHandle->TxLength--;
+        (uint16_t *)pSPIHandle->pTxBuffer++;
+
+    } else {
+
+        // 8 bit DFF
+        pSPIHandle->pSPI->DR = *pSPIHandle->pTxBuffer;
+        pSPIHandle->TxLength--;
+        pSPIHandle->pTxBuffer++;
+
+    }
+
+    if (!pSPIHandle->TxLength) {
+
+        CloseTransmission(pSPIHandle);
+        SPI_ApplicationEventCallback(pSPIHandle, SPI_EVENT_TX_CMPLT);
+    }
+
+}
+
+
+static void spi_rxne_interrupt_handle(SPI_Handle_t *pSPIHandle) {
+
+    // 2. Check the DFF bit in CR1
+    if ((pSPIHandle->pSPI->CR1 & (1 << SPI_CR1_DFF))) {
+
+        // 16 bit DFF
+        // 1. load the data in to the DR
+        *((uint16_t *)pSPIHandle->pRxBuffer) = pSPIHandle->pSPI->DR;
+        pSPIHandle->RxLength -= 2;
+        (uint16_t *)pSPIHandle->pRxBuffer--;
+        (uint16_t *)pSPIHandle->pRxBuffer--;
+
+    } else {
+
+        // 8 bit DFF
+        *pSPIHandle->pRxBuffer = (uint8_t)pSPIHandle->pSPI->DR;
+        pSPIHandle->RxLength--;
+        pSPIHandle->pRxBuffer--;
+
+    }
+
+    if (!pSPIHandle->RxLength) {
+
+        CloseReception(pSPIHandle);
+
+        SPI_ApplicationEventCallback(pSPIHandle, SPI_EVENT_RX_CMPLT);
+    }
+
+}
+static void spi_ovr_err_interrupt_handle(SPI_Handle_t *pSPIHandle) {
+
+    uint8_t temp;
+
+    // 1. clear the ovr flag
+    if (pSPIHandle->TxState != SPI_BUSY_IN_TX) {
+        temp = pSPIHandle->pSPI->DR;
+        temp = pSPIHandle->pSPI->SR;
+    }
+    (void) temp;
+
+    // 2. inform the application
+    SPI_ApplicationEventCallback(pSPIHandle, SPI_EVENT_OVR_ERR);
+
+
+}
+
+
+/*********************************************************************
+ * @fn      		  - SPI_SSOEConfig
+ *
+ * @brief             - This function enables or disables slave select output for the given SPI port
+ *
+ * @param[in]         - base address of the SPI peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            - none
+ *
+ * @Note              - none
+ */
+void CloseTransmission(SPI_Handle_t *pSPIHandle) {
+    // TxLength is zero, so close the spi transmission and inform the application that
+    // Tx is over
+
+    // this prevents interrupts from setting up of TXE flag
+    pSPIHandle->pSPI->CR2 &= ~(1 << SPI_CR2_TXNEIE);
+    pSPIHandle->pTxBuffer = NULL;
+    pSPIHandle->TxLength = 0;
+    pSPIHandle->TxState = SPI_READY;
+}
+
+
+/*********************************************************************
+ * @fn      		  - SPI_SSOEConfig
+ *
+ * @brief             - This function enables or disables slave select output for the given SPI port
+ *
+ * @param[in]         - base address of the SPI peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            - none
+ *
+ * @Note              - none
+ */
+void CloseReception(SPI_Handle_t *pSPIHandle) {
+
+    // RxLength is zero, so close the spi transmission and inform the application that
+    // Rx is over
+
+    // this prevents interrupts from setting up of RXE flag
+    pSPIHandle->pSPI->CR2 &= ~(1 << SPI_CR2_RXNEIE);
+    pSPIHandle->pTxBuffer = NULL;
+    pSPIHandle->RxLength = 0;
+    pSPIHandle->RxState = SPI_READY;
+
+}
+
+
+/*********************************************************************
+ * @fn      		  - SPI_SSOEConfig
+ *
+ * @brief             - This function enables or disables slave select output for the given SPI port
+ *
+ * @param[in]         - base address of the SPI peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            - none
+ *
+ * @Note              - none
+ */
+void SPI_ClearOVRFlag(SPI_RegDef_t *pSPIx) {
+
+    uint8_t temp;
+
+    // 1. clear the ovr flag
+    temp = pSPIx->DR;
+    temp = pSPIx->SR;
+
+    (void) temp;
+
+}
+
+
+
 
 
 /*********************************************************************
@@ -328,13 +667,11 @@ void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
  *
  * @Note              - none
  */
-void SPI_IRQHandling(SPI_Handle_t *pSPIHandle);
+
 
 /*
  * Other Peripheral Control APIs
  */
-
-
 /*********************************************************************
  * @fn      		  - SPI_PeripheralControl
  *
@@ -406,3 +743,22 @@ void SPI_SSOEConfig(SPI_RegDef_t *pSPIx, uint8_t enOrDi) {
 
 }
 
+
+/*********************************************************************
+ * @fn      		  - SPI_ApplicationEventCallback
+ *
+ * @brief             - This function enables or disables slave select output for the given SPI port
+ *
+ * @param[in]         - base address of the SPI peripheral and SPI config
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            - none
+ *
+ * @Note              - none
+ */
+__weak void SPI_ApplicationEventCallback(SPI_Handle_t *pSPIHandle, uint8_t AppEv) {
+
+    // This is a weak implementation. the user application may override this function
+
+}
