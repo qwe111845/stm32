3a88f507f8f09c5711664bdf2a3f6292267f1f2d
qwe111845 <qwe111845@gmail.com>
2022-07-24 02:45:58 +0800

    feat: Inter Integrated Circuit API-I2C 各 API 實作
    新增原因:
    1. 實作 I2C 需要的 function 及修改 I2C sendData。
    
    新增項目:
    1. 增加 I2C FLAG，方便取得SR1 狀態。
    2. 修改 I2C_RegDef_t 為 I2C_Handle_t

--- stm32f429i_drivers/drivers/Inc/stm32f429zi_i2c_driver.h
+++ stm32f429i_drivers/drivers/Inc/stm32f429zi_i2c_driver.h
@@ -47,6 +47,23 @@ typedef struct {
 #define I2C_FM_DUTY_16_9 1
 
 
+/*
+ * @I2C related status flags definitions
+ */
+#define I2C_FLAG_SB          (1 << I2C_SR1_SB)
+#define I2C_FLAG_ADDR        (1 << I2C_SR1_ADDR)
+#define I2C_FLAG_BTF         (1 << I2C_SR1_BTF)
+#define I2C_FLAG_STOPF       (1 << I2C_SR1_STOPF)
+#define I2C_FLAG_RXNE        (1 << I2C_SR1_RXNE)
+#define I2C_FLAG_TXE         (1 << I2C_SR1_TxE)
+#define I2C_FLAG_BERR        (1 << I2C_SR1_BERR)
+#define I2C_FLAG_ARLO        (1 << I2C_SR1_ARLO)
+#define I2C_FLAG_AF          (1 << I2C_SR1_AF)
+#define I2C_FLAG_OVR         (1 << I2C_SR1_OVR)
+#define I2C_FLAG_PECERR      (1 << I2C_SR1_PECERR)
+#define I2C_FLAG_TIME_OUT    (1 << I2C_SR1_TIME_OUT)
+#define I2C_FLAG_SMB_ALERT   (1 << I2C_SR1_SMB_ALERT)
+
 /********************************************************************************************
  *                             APIs supported by this driver
  *             For more information about the APIs check the function definitions
@@ -67,8 +84,8 @@ void I2C_DeInit(I2C_RegDef_t *pI2Cx);
 /*
  * Data Send and Receive
  */
-void I2C_SendData(I2C_RegDef_t *pI2Cx, uint8_t *pTxBuffer, uint32_t length);
-void I2C_ReceiveData(I2C_RegDef_t *pI2Cx, uint8_t *pRxBuffer, uint32_t length);
+void I2C_MasterSendData(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t length, uint8_t slaveAddress);
+void I2C_ReceiveData(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t length);
 
 
 /*
