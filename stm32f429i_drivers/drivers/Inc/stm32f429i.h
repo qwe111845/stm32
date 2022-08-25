d48e9fd1325a3af94314d4e217c12fd8a7b5c6fe
qwe111845 <qwe111845@gmail.com>
2022-08-25 14:19:28 +0800

    style: stmf429i API-修改 I2C 全域變數名稱
    修改原因:
    1. RxNE 有小寫，不符合 naming convention。

--- stm32f429i_drivers/drivers/Inc/stm32f429i.h
+++ stm32f429i_drivers/drivers/Inc/stm32f429i.h
@@ -555,8 +555,8 @@ typedef struct {
 #define I2C_SR1_BTF       2
 #define I2C_SR1_ADD10     3
 #define I2C_SR1_STOPF     4
-#define I2C_SR1_RxNE      6
-#define I2C_SR1_TxE       7
+#define I2C_SR1_RXNE      6
+#define I2C_SR1_TXE       7
 #define I2C_SR1_BERR      8
 #define I2C_SR1_ARLO      9
 #define I2C_SR1_AF        10
